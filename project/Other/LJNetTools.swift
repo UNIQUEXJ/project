b86c3d4da87c8aa607b3ab8e989e9a033de675cd
jiangliu <jiangliu@mengyangsoft.com>
2017-09-22 15:03:56 +0800

    修改

--- project/Other/LJNetTools.swift
+++ project/Other/LJNetTools.swift
@@ -12,11 +12,11 @@ import ObjectMapper
 import SwiftyJSON
 import MBProgressHUD
 
-let baseURL = "http://www.window18.com:8000/api/"
+let baseURL = "http://www.window18.com/api/"
 //let baseURL = "http://139.196.31.161:8025/a/mobileapi/"
 
 struct APIPath {
-    static let loginPath = "http://www.window18.com:8666/api/MobileInterface.asmx/Login" // 登录
+    static let loginPath = "http://www.window18.com/api/MobileInterface.asmx/Login" // 登录
     static let logout = "Customers/logout" // 登出
     static let companyList = "Customers/GetUsersList" //获取公司列表
 }
